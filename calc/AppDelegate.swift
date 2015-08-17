a5242ce0e6bf08b09f1aa13f8cdd3b46a5606df6
Zhenwei <zhenwei@AppledeMacBook-Air.local>
2015-08-17 17:11:33 +0800

    Initial Commit

---
+++ calc/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  calc
+//
+//  Created by Zhenwei on 15/8/17.
+//  Copyright (c) 2015年 Zhenwei. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ calc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by Zhenwei on 15/8/17.
+//  Copyright (c) 2015年 Zhenwei. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
